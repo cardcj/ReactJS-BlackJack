f1e2302a883435e4850df54993e6fe75d2529e98
Chris Cardwell <chrisjcardwell@gmail.com>
2019-02-05 11:22:29 +0000

    Added basic css to enable the html and body elements to take up 100% of the height of the window. Also created the assets directory that contains the data directory currently containing the deck.

---
+++ src/assets/data/deck.js
@@ -0,0 +1,262 @@
+export const products = [
+    {
+        id: 0,
+        value: "Ace",
+        suit: "Spades"
+    },
+    {
+        id: 1,
+        value: 2,
+        suit: "Spades"
+    },
+    {
+        id: 2,
+        value: 3,
+        suit: "Spades"
+    },
+    {
+        id: 3,
+        value: 4,
+        suit: "Spades"
+    },
+    {
+        id: 4,
+        value: 5,
+        suit: "Spades"
+    },
+    {
+        id: 5,
+        value: 6,
+        suit: "Spades"
+    },
+    {
+        id: 6,
+        value: 7,
+        suit: "Spades"
+    },
+    {
+        id: 7,
+        value: 8,
+        suit: "Spades"
+    },
+    {
+        id: 8,
+        value: 9,
+        suit: "Spades"
+    },
+    {
+        id: 9,
+        value: 10,
+        suit: "Spades"
+    },
+    {
+        id: 10,
+        value: "Jack",
+        suit: "Spades"
+    },
+    {
+        id: 11,
+        value: "Queen",
+        suit: "Spades"
+    },
+    {
+        id: 12,
+        value: "King",
+        suit: "Spades"
+    },
+    {
+        id: 13,
+        value: "Ace",
+        suit: "Hearts"
+    },
+    {
+        id: 14,
+        value: 2,
+        suit: "Hearts"
+    },
+    {
+        id: 15,
+        value: 3,
+        suit: "Hearts"
+    },
+    {
+        id: 16,
+        value: 4,
+        suit: "Hearts"
+    },
+    {
+        id: 17,
+        value: 5,
+        suit: "Hearts"
+    },
+    {
+        id: 18,
+        value: 6,
+        suit: "Hearts"
+    },
+    {
+        id: 19,
+        value: 7,
+        suit: "Hearts"
+    },
+    {
+        id: 20,
+        value: 8,
+        suit: "Hearts"
+    },
+    {
+        id: 21,
+        value: 9,
+        suit: "Hearts"
+    },
+    {
+        id: 22,
+        value: 10,
+        suit: "Hearts"
+    },
+    {
+        id: 23,
+        value: "Jack",
+        suit: "Hearts"
+    },
+    {
+        id: 24,
+        value: "Queen",
+        suit: "Hearts"
+    },
+    {
+        id: 25,
+        value: "King",
+        suit: "Hearts"
+    },
+    {
+        id: 26,
+        value: "Ace",
+        suit: "Clubs"
+    },
+    {
+        id: 27,
+        value: 2,
+        suit: "Clubs"
+    },
+    {
+        id: 28,
+        value: 3,
+        suit: "Clubs"
+    },
+    {
+        id: 29,
+        value: 4,
+        suit: "Clubs"
+    },
+    {
+        id: 30,
+        value: 5,
+        suit: "Clubs"
+    },
+    {
+        id: 31,
+        value: 6,
+        suit: "Clubs"
+    },
+    {
+        id: 32,
+        value: 7,
+        suit: "Clubs"
+    },
+    {
+        id: 33,
+        value: 8,
+        suit: "Clubs"
+    },
+    {
+        id: 34,
+        value: 9,
+        suit: "Clubs"
+    },
+    {
+        id: 35,
+        value: 10,
+        suit: "Clubs"
+    },
+    {
+        id: 36,
+        value: "Jack",
+        suit: "Clubs"
+    },
+    {
+        id: 37,
+        value: "Queen",
+        suit: "Clubs"
+    },
+    {
+        id: 38,
+        value: "King",
+        suit: "Clubs"
+    },
+    {
+        id: 39,
+        value: "Ace",
+        suit: "Diamonds"
+    },
+    {
+        id: 40,
+        value: 2,
+        suit: "Diamonds"
+    },
+    {
+        id: 41,
+        value: 3,
+        suit: "Diamonds"
+    },
+    {
+        id: 42,
+        value: 4,
+        suit: "Diamonds"
+    },
+    {
+        id: 43,
+        value: 5,
+        suit: "Diamonds"
+    },
+    {
+        id: 44,
+        value: 6,
+        suit: "Diamonds"
+    },
+    {
+        id: 45,
+        value: 7,
+        suit: "Diamonds"
+    },
+    {
+        id: 46,
+        value: 8,
+        suit: "Diamonds"
+    },
+    {
+        id: 47,
+        value: 9,
+        suit: "Diamonds"
+    },
+    {
+        id: 48,
+        value: 10,
+        suit: "Diamonds"
+    },
+    {
+        id: 49,
+        value: "Jack",
+        suit: "Diamonds"
+    },
+    {
+        id: 50,
+        value: "Queen",
+        suit: "Diamonds"
+    },
+    {
+        id: 51,
+        value: "King",
+        suit: "Diamonds"
+    },
+]
